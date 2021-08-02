11ece39866f1910d1f4d546cb5ac3f1f9b2cff9f
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2021-08-02 11:05:24 -0700

    Some points have been done

--- 2/commerce/auctions/views.py
+++ 2/commerce/auctions/views.py
@@ -1,14 +1,51 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.urls import reverse
+from django.core.validators import MinValueValidator
+from django import forms
 
-from .models import User
+from .models import User, Listing
+
+
+class ListingForm(forms.Form):
+    title = forms.CharField(max_length=64, label="Title")
+    description = forms.CharField(widget=forms.Textarea, max_length=120, 
+            label="Description")
+    starting_bid = forms.FloatField(localize=False, validators=[
+        MinValueValidator(0.9)], label="Staring Bid")
+    image = forms.CharField(label="Image Link", max_length=240)
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", 
+            {'listings': Listing.objects.filter(closed= False).all()})
+
+
+def view_listing(request, listing_id):
+    # TODO show error saying listing was not found if None
+    listing = Listing.objects.get(id=listing_id)
+    if listing is not None:
+        return render(request, 'auctions/view_listing.html', {'listing': 
+                                                                listing})
+    # return to home page showing error.
+    return HttpRedirectResponse(reverse('index'))
+
+
+def add_listing(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            # add a new listing
+            t = form.cleaned_data.get("title")
+            d = form.cleaned_data.get("description")
+            b = form.cleaned_data.get("starting_bid")
+            i = form.cleaned_data.get("image")
+            Listing.objects.create(title=t, description=d, starting_bid=b, image=i)
+            return HttpResponseRedirect(reverse("index"))
+    return render(request, 'auctions/add_listing.html', {
+        'form': ListingForm()})
 
 
 def login_view(request):
