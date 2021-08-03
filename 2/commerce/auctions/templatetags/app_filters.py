320ed5c6184f31abec4f4eda3ce55611702491cb
Mario Molinito <unknownliteracy729videogames0@gmail.com>
2021-08-03 11:23:50 -0700

    Before final fix up

--- 2/commerce/auctions/templatetags/app_filters.py
+++ 2/commerce/auctions/templatetags/app_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from auctions.models import Categories
 
 
 register = template.Library()
@@ -7,3 +8,11 @@ register = template.Library()
 @register.filter(name='mod_three')
 def mod_three(value):
     return (value - 1) % 3
+
+
+@register.filter(name='contains_query')
+def contains_query(value):
+    category = value.get('category')
+    if category is None:
+        return False
+    return Categories.objects.get(id=category)
